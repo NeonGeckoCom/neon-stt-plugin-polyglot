d2ad940310cf6b6e0e9a753e0040d09e7abcab4b
Mariia Razno <mariia@neon.ai>
2022-02-16 18:09:46 +0200

    config path changed

--- neon_stt_plugin_polyglot/__init__.py
+++ neon_stt_plugin_polyglot/__init__.py
@@ -78,7 +78,7 @@ class PolyglotSTT(STT):
             os.makedirs(folder)
             LOG.info(f"Downloading model for polyglot ...")
             LOG.info("this might take a while")
-            with open(os.environ.get('SFTP_CREDS_PATH', '../sftp_config.json')) as f:
+            with open(os.environ.get('SFTP_CREDS_PATH', '/tests/sftp_config.json')) as f:
                 sftp_creds = json.load(f)
                 NeonSFTPConnector.connector = NeonSFTPConnector(**sftp_creds)
             get_graph = '/polyglot/'+self.lang+'/output_graph.pbmm'
