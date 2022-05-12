d9951887999dd32b4fbfa03ec38552bee16e1c75
Daniel McKnight <daniel@neon.ai>
2022-05-12 13:37:22 -0700

    Update entrypoint to avoid conflicts with TTS plugin

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_requirements(requirements_filename: str):
     return requirements
 
 
-PLUGIN_ENTRY_POINT = 'coqui = neon_stt_plugin_coqui:CoquiSTT'
+PLUGIN_ENTRY_POINT = 'neon-stt-plugin-coqui = neon_stt_plugin_coqui:CoquiSTT'
 
 with open("README.md", "r") as f:
     long_description = f.read()
