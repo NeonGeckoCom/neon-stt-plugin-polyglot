99d689515dcca4ed05fa380ceff8daf2c06affd0
NeonMariia <mariia@neon.ai>
2022-06-03 05:08:57 -0700

    audio names fixes, date to folder name

--- tests/test_stt.py
+++ tests/test_stt.py
@@ -30,6 +30,7 @@ import unittest
 import pandas as pd
 from jiwer import cer
 from timeit import default_timer as timer
+from datetime import date
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 TEST_PATH_EN = os.path.join(ROOT_DIR, "test_audio/en")
@@ -40,39 +41,44 @@ TEST_PATH_DE = os.path.join(ROOT_DIR, "test_audio/de")
 TEST_PATH_UK = os.path.join(ROOT_DIR, "test_audio/uk")
 TEST_PATH_CNH = os.path.join(ROOT_DIR, "test_audio/cnh")
 
+today = date.today()
+reports_folder = ROOT_DIR+'/test_reports_'+str(today)+'/'
+if os.path.isdir(reports_folder):
+    LOG.info('Reports dir exists')
+else:
+    os.mkdir(reports_folder)
+
 
 class TestGetSTT(unittest.TestCase):
 
-
     def evaluation_script(self, folder, lang, report_name):
+        today = date.today()
         ground_truth = []
         hypothesis = []
         df_list = []
         for file in os.listdir(folder):
-            if file not in ['male', 'female', '.DS_Store']:
-                LOG.info(file)
-                transcription = ' '.join(file[:-4].split('_')).lower()
-                ground_truth.append(transcription)
-                path = folder+'/'+file
-                stt = CoquiSTT(lang)
-                LOG.info('Running inference.')
-                inference_start = timer()
-                audio_length, audio_data = stt.get_audio_data(path)
-                text = stt.execute(audio_data)
-                LOG.info("Transcription: "+text)
-                inference_end = timer() - inference_start
-                LOG.info('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_length))
-                # model's output
-                # translit = neon_utils.parse_utils.transliteration(transcription, text, 'pl')
-                # LOG.info("Transcription transliterated: "+translit)
-                hypothesis.append(text)
-                df_list.append([transcription, text, audio_length, inference_end])
+            transcription = ' '.join(file[:-4].split('_')).lower()
+            ground_truth.append(transcription)
+            path = folder+'/'+file
+            stt = CoquiSTT(lang)
+            LOG.info('Running inference.')
+            inference_start = timer()
+            audio_length, audio_data = stt.get_audio_data(path)
+            text = stt.execute(audio_data)
+            inference_end = timer() - inference_start
+            LOG.info('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_length))
+            # model's output
+            translit = neon_utils.parse_utils.transliteration(transcription, text, lang)
+            LOG.info("Transcription transliterated: "+translit)
+            hypothesis.append(translit)
+            df_list.append([transcription, text, audio_length, inference_end])
         error = cer(ground_truth, hypothesis)
+        self.assertTrue(error < 0.3)
         LOG.info('Input: {}\nOutput:{}\nWER: {}'.format(ground_truth, hypothesis, error))
         #creating dataframe
         df_list.append(['CER', error, '', ''])
         dataframe = pd.DataFrame(df_list, columns=['ground_truth', 'hypothesis', 'audio_length', 'inference_end'])
-        df_path = ROOT_DIR+'/test_reports/'+report_name+'.csv'
+        df_path = ROOT_DIR+'/test_reports_'+str(today)+'/'+report_name+'.csv'
         dataframe.to_csv(df_path)
 
 
@@ -106,7 +112,7 @@ class TestGetSTT(unittest.TestCase):
         male_folder = TEST_PATH_PL+'/male'
         female_folder = TEST_PATH_PL+'/female'
         self.evaluation_script(male_folder, 'pl', 'pl_report_male')
-        # self.evaluation_script(female_folder, 'pl', 'pl_report_female')
+        self.evaluation_script(female_folder, 'pl', 'pl_report_female')
 
 
 if __name__ == '__main__':
